c594cdf88f86d36a955263caea840170dac7dbfb
Luscha <matteo.roggia@gmail.com>
2017-05-08 20:07:19 +0200

    Added toBytes32Array to the string lib

--- lib/stringUtils.sol
+++ lib/stringUtils.sol
@@ -7,4 +7,12 @@ library StringUtils {
       ret |= bytes32(inputBytes[i]) >> (i * 8);
     }
   }
+
+  function toBytes32Array(string self) constant returns (bytes32[] ret) {
+    bytes memory inputBytes = bytes(self);
+    ret = new bytes32[]((inputBytes.length / 32) + 1);
+    for (uint64 i = 0; i < inputBytes.length; i++) {
+      ret[i / 32] |= bytes32(inputBytes[i]) >> ((i % 32) * 8);
+    }
+  }
 }
