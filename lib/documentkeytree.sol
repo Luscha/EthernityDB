42c3b08d360c050acb37b2212ad0cd10fc75027a
Luscha <matteo.roggia@gmail.com>
2017-05-15 11:10:53 +0200

    Tried to change the tree library with the struct from storage to memory (failed. documentkeytreeflat is the rightr library)

--- lib/documentkeytree.sol
+++ lib/documentkeytree.sol
@@ -15,21 +15,20 @@ library DocumentKeyTree {
     bool isInit;
   }
 
-  function newRoot() internal returns (DocumentKeyRoot root) {
+  function newRoot() internal returns (DocumentKeyRoot memory root) {
       root = DocumentKeyRoot({currentNode: DocumentKeyNode({isInit: false}), isCurrent: true});
   }
 
-  function addChild(DocumentKeyRoot storage root, bytes32 nodeName) internal returns (DocumentKeyNode storage newNode) {
+  function addChild(DocumentKeyRoot storage root, bytes32 nodeName) internal {
     if (root.isCurrent == true) {
-        newNode = root.children[nodeName];
+        root.children[nodeName] = DocumentKeyNode({isInit: true});
         root.isCurrent = false;
+        root.currentNode.children[nodeName] = root.children[nodeName];
     } else {
-        newNode = root.currentNode.children[nodeName];
-        newNode.parent[true] = root.currentNode;
+        root.currentNode.children[nodeName] = DocumentKeyNode({isInit: true});
+        root.currentNode.children[nodeName].parent[true] = root.currentNode;
+        root.currentNode = root.currentNode.children[nodeName];
     }
-
-    newNode.isInit = true;
-    root.currentNode = newNode;
   }
 
   function upToParent(DocumentKeyRoot storage root) internal {
@@ -43,6 +42,48 @@ library DocumentKeyTree {
   }
 
   function setKeyIndex(DocumentKeyRoot storage root, bytes32 k, uint64 i) internal {
-    root.currentNode.keyIndex[k] = i;
+    if (root.isCurrent == true) {
+      root.keyIndex[k] = i;
+    } else {
+      root.currentNode.keyIndex[k] = i;
+    }
+  }
+
+  function selectRoot(DocumentKeyRoot storage root) internal {
+    root.isCurrent = true;
+  }
+
+  function selectKey(DocumentKeyRoot storage root, bytes32 k) internal returns (bool, uint64) {
+    if (root.isCurrent == true) {
+        if (root.keyIndex[k] == 0) {
+          return (false, 0);
+        } else {
+          return (true, root.keyIndex[k]);
+        }
+    } else {
+      if (root.currentNode.keyIndex[k] == 0) {
+        return (false, 0);
+      } else {
+        return (true, root.currentNode.keyIndex[k]);
+      }
+    }
+  }
+
+  function selectChildren(DocumentKeyRoot storage root, bytes32 k) internal returns (bool) {
+    if (root.isCurrent == true) {
+        if (root.children[k].isInit == false) {
+          return false;
+        } else {
+          root.currentNode = root.children[k];
+          return true;
+        }
+    } else {
+      if (root.currentNode.children[k].isInit == false) {
+        return false;
+      } else {
+        root.currentNode = root.currentNode.children[k];
+        return true;
+      }
+    }
   }
 }
