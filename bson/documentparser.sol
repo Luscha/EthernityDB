e34a6722b44a73123833a19c6b2a2eb4c8197735
Luscha <matteo.roggia@gmail.com>
2017-05-21 19:34:08 +0200

    function to get the combined name changed from private to internals

--- bson/documentparser.sol
+++ bson/documentparser.sol
@@ -5,6 +5,7 @@ import "lib/treeflat.sol";
 library DocumentParser {
   using BytesUtils for byte[];
   using DocumentParser for byte[];
+  using DocumentParser for bytes32;
   using TreeFlat for TreeFlat.TreeRoot;
 
   function nextKeyValue(byte[] memory d, uint32 i) constant internal returns (uint8 t, bytes8 n, uint32 l, uint32 s) {
@@ -92,13 +93,13 @@ library DocumentParser {
 
   // This function combines the key name with his type so that later is possible to search for a
   // particular key with a given type in a single lookup
-  function getCombinedNameType(bytes32 n, uint8 t) constant private returns (bytes32 comb){
+  function getCombinedNameType(bytes32 n, uint8 t) constant internal returns (bytes32 comb){
     comb = sha3(n, t);
     comb = comb >> 8 | bytes32(t) << (8 * 30);
   }
 
   // Like above but it returns only a 8 byte key
-  function getCombinedNameType8(bytes32 n, uint8 t) constant private returns (bytes8 comb){
+  function getCombinedNameType8(bytes32 n, uint8 t) constant internal returns (bytes8 comb){
     comb = bytes8(sha3(n, t));
     comb = (comb >> 8) | (bytes8(t) << 48);
   }
