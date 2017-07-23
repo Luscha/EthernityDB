e7173381aaba8d9cad6f9b8914a50ac0ff556431
Luscha <matteo.roggia@gmail.com>
2017-07-23 12:27:00 +0200

    Fixed and refactored the flag library

--- lib/flag.sol
+++ lib/flag.sol
@@ -2,18 +2,18 @@ pragma solidity ^0.4.11;
 
 library Flag {
   function setBit(uint32 self, uint8 bit) returns (uint32) {
-    return self | (0x00000000000000000000000000000001 << bit);
+    return self | (uint32(1) << bit);
   }
 
   function removeBit(uint32 self, uint8 bit) returns (uint32) {
-    return self & (~(0x00000000000000000000000000000001 << bit));
+    return self & ((uint32(1) << bit) ^ uint32(-1));
   }
 
   function toggleBit(uint32 self, uint8 bit) returns (uint32) {
-    return self ^ (0x00000000000000000000000000000001 << bit);
+    return self ^ (uint32(1) << bit);
   }
 
   function isBit(uint32 self, uint8 bit) returns (bool) {
-    return self & (0x00000000000000000000000000000001 << bit) == (0x00000000000000000000000000000001 << bit);
+    return self & (uint32(1) << bit) == (uint32(1) << bit);
   }
 }
