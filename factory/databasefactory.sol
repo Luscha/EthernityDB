f29549e97df23ab659bb0d66f5ce76608222e910
Luscha <matteo.roggia@gmail.com>
2017-08-20 04:40:11 +0200

    Added event to track new database in the database factory

--- factory/databasefactory.sol
+++ factory/databasefactory.sol
@@ -5,11 +5,14 @@ import "database.sol";
 contract DatabaseFactory is DatabaseFactoryAbstract {
   string private version = "master-1.0.0";
 
+  event databaseCreated(address owner, string name);
+
   function getVersion() constant returns (string) {
     return version;
   }
 
   function createDatabase(string name, bool[] flags, DriverAbstract driver, CollectionFactoryAbstract cf) returns (DBAbstract) {
+    databaseCreated(tx.origin, name);
     return new Database(name, flags, driver, cf);
   }
 }
